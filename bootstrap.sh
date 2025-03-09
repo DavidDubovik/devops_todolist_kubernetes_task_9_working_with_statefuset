be588633f8e86addbd226a5e35574dc85b152716
David <DavidDubovik19@gmail.com>
2025-03-09 22:50:51 +0200

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+kubectl apply -f .infrastructure/secret.yml
+kubectl apply -f .infrastructure/statefulSet.yml
+kubectl apply -f .infrastructure/todo-app-secret.yml
+kubectl apply -f .infrastructure/deployment.yml
+
