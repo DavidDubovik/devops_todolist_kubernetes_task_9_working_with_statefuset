be1326b6f55167c2d8e26edd1acb97ab9384a1dc
David <DavidDubovik19@gmail.com>
2025-03-16 18:00:40 +0200

    fix 2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f .infrastructure/secret.yml
+kubectl apply -f .infrastructure/clusterIp.yml
 kubectl apply -f .infrastructure/statefulSet.yml
 kubectl apply -f .infrastructure/secret.yml
 kubectl apply -f .infrastructure/deployment.yml
